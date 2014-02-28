60b9c53e9eebae69bb60c7ce4f35d8f25e079842
Dustin Carlino <dabreegster@gmail.com>
2014-02-28 13:31:19 -0600

    reflection to get case class fields in order (thanks to SO)

--- NestBrowser.scala
+++ NestBrowser.scala
@@ -1,3 +1,6 @@
+import scala.reflect.runtime.{universe => ru}
+import scala.collection.immutable
+
 sealed trait Tree
 case class CaseClass(fields: List[(String, Tree)]) extends Tree
 case class Mapping(map: Map[String, Tree]) extends Tree
@@ -12,9 +15,27 @@ case class Scenario(agents: List[Agent], modes: Array[String], name: String, id:
 case class Agent(num: Int, times: Array[Double])
 
 object Test {
+  // from
+  // http://stackoverflow.com/questions/16079113/scala-2-10-reflection-how-do-i-extract-the-field-values-from-a-case-class
+  def caseClassParamsOf[T: ru.TypeTag]: immutable.ListMap[String, ru.Type] = {
+    val tpe = ru.typeOf[T]
+    val constructorSymbol = tpe.declaration(ru.nme.CONSTRUCTOR)
+    val defaultConstructor =
+      if (constructorSymbol.isMethod) constructorSymbol.asMethod
+      else {
+        val ctors = constructorSymbol.asTerm.alternatives
+        ctors.map { _.asMethod }.find { _.isPrimaryConstructor }.get
+      }
+
+    return immutable.ListMap[String, ru.Type]() ++ defaultConstructor.paramss.reduceLeft(_ ++ _).map {
+      sym => sym.name.toString -> tpe.member(sym.name).asMethod.returnType
+    }
+  }
+
   def main(args: Array[String]) {
     val a1 = Agent(1, Array(2.0, 5.0))
     val a2 = Agent(2, Array(3.0, 2.3))
     val s = Scenario(List(a1, a2), Array("baseline", "tolls"), "trial", 42)
+    println(caseClassParamsOf[Scenario])
   }
 }
--- build.sbt
+++ build.sbt
@@ -7,5 +7,7 @@ scalaVersion := "2.10.3"
 // src/main/scala is too verbose
 scalaSource in Compile := baseDirectory.value
 
+libraryDependencies += "org.scala-lang" % "scala-reflect" % scalaVersion.value
+
 // Be able to collect all dependencies
 packSettings
