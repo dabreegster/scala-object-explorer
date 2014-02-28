e731bf49e362791ee6636b113cb7561aab5f1bd7
Dustin Carlino <dabreegster@gmail.com>
2014-02-28 14:39:04 -0600

    a recursive dump fxn

--- NestBrowser.scala
+++ NestBrowser.scala
@@ -17,8 +17,7 @@ case class Agent(num: Int, times: Array[Double])
 object Test {
   // from
   // http://stackoverflow.com/questions/16079113/scala-2-10-reflection-how-do-i-extract-the-field-values-from-a-case-class
-  def caseClassParamsOf[T: ru.TypeTag]: immutable.ListMap[String, ru.Type] = {
-    val tpe = ru.typeOf[T]
+  def caseClassParamsOf(tpe: ru.Type): immutable.ListMap[String, ru.Type] = {
     val constructorSymbol = tpe.declaration(ru.nme.CONSTRUCTOR)
     val defaultConstructor =
       if (constructorSymbol.isMethod) constructorSymbol.asMethod
@@ -39,13 +38,37 @@ object Test {
     return im.reflectField(term).get
   }
 
+  def dump(obj: Any, obj_type: ru.Type, level: Int) {
+    obj match {
+      case ls: List[Any] =>
+        // get parameter type from
+        // http://stackoverflow.com/questions/12842729/finding-type-parameters-via-reflection-in-scala-2-10
+        ls.foreach(x => dump(x, obj_type.asInstanceOf[ru.TypeRefApi].args.head, level + 1))
+      case ls: Array[_] =>
+        ls.foreach(x => dump(x, obj_type.asInstanceOf[ru.TypeRefApi].args.head, level + 1))
+      // breaks on caseClassParamsOf
+      case x: String => println(("  " * level) + obj)
+      case _ => {
+        // TODO messy. but seems to work.
+        val fields = caseClassParamsOf(obj_type)
+        if (fields.isEmpty) {
+          // it's a primitive/leaf
+          println(("  " * level) + obj)
+        } else {
+          for ((name, tpe) <- fields) {
+            val value = get_field(obj, obj_type, name)
+            println(("  " * level) + name + " = ")
+            dump(value, tpe, level + 1)
+          }
+        }
+      }
+    }
+  }
+
   def main(args: Array[String]) {
     val a1 = Agent(1, Array(2.0, 5.0))
     val a2 = Agent(2, Array(3.0, 2.3))
     val s = Scenario(List(a1, a2), Array("baseline", "tolls"), "trial", 42)
-    for ((name, tpe) <- caseClassParamsOf[Scenario]) {
-      val value = get_field(s, ru.typeOf[Scenario], name)
-      println(s"$name ($tpe) = $value")
-    }
+    dump(s, ru.typeOf[Scenario], 0)
   }
 }
